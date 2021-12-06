4159506134cbc197922ddd0817aede2fd0787443
Albert <albert.ohanyan.01@gmail.com>
2021-12-06 18:33:16 +0400

    Account example with delegates

---
+++ AccountWithDelegate/Account.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountWithDelegate
+{
+    public class Account
+    {
+        int _sum;
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+
+        public int CurrentSum
+        {
+            get { return _sum; }
+        }
+
+        public void Put(int sum)
+        {
+            _sum += sum;
+        }
+
+        public delegate void AccountStateHandler(string message);
+        AccountStateHandler _del;
+
+        public void RegisterHandler(AccountStateHandler del)
+        {
+            _del = del;
+        }
+
+        public void Withdraw(int sum)
+        {
+            if (sum<=_sum)
+            {
+                _sum -= sum;
+                if (_del!=null)
+                {
+                    _del($"The amount {sum} has been withdrawn from the account");
+                }
+            }
+            else
+            {
+                if (_del!=null)
+                {
+                    _del("There is not enough money in the account");
+                }
+            }
+        }
+    }
+}
---
+++ AccountWithDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountWithDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(500);
+            account.RegisterHandler(new Account.AccountStateHandler(Show_Message));
+            account.Withdraw(300);
+
+            int money=account.CurrentSum;
+            Console.WriteLine(money);
+
+            account.Withdraw(250);
+
+            Console.ReadLine();
+        }
+        private static void Show_Message(String message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
